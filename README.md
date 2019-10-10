d611414c6b0a733294b23ffc636ae8cdc4c822dc
qutang <tqshelly@gmail.com>
2019-10-10 12:26:20 -0400

    Init project with poetry

--- README.md
+++
@@ -1,2 +0,0 @@
-# arus-stream-metawear
-arus package plugin to build the metawear stream
---
+++ tests/test_arus_stream_metawear.py
@@ -0,0 +1,5 @@
+from arus_stream_metawear import __version__
+
+
+def test_version():
+    assert __version__ == '0.1.0'
